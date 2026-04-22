97034e9be71eb02193d4f0e028fac3b251cdf910
sreeram780 <sreenu.ram2@gmail.com>
2026-04-22 10:21:28 +0530

    series and data frames in pandas

---
+++ 1.1-series.py
@@ -0,0 +1,21 @@
+import pandas as pd
+# Series
+data = ['Steve', '35', 'Male', '3.5']
+series = pd.Series(data, index=['Name', 'Age', 'Gender', 'Rating'])
+print(series)
+
+# DataFrame
+# Data represented as a dictionary
+data = {
+    'Name': ['Steve', 'Lia', 'Vin', 'Katie'],
+    'Age': [32, 28, 45, 38],
+    'Gender': ['Male', 'Female', 'Male', 'Female'],
+    'Rating': [3.45, 4.6, 3.9, 2.78]
+}
+# Creating the DataFrame
+df = pd.DataFrame(data)
+# Display the DataFrame
+print(df)
+
+# Display a Series within a DataFrame
+print(df['Name'])
