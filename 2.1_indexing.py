41aaed28ea96b613d697a9bf8e09c21e92a3831a
sreeram780 <sreenu.ram2@gmail.com>
2026-04-23 19:10:41 +0530

    - interval indexing

--- 2.1_indexing.py
+++ 2.1_indexing.py
@@ -30,6 +30,14 @@ print()
 
 # MultiIndex
 # IntervalIndex
+import pandas as pd
+# Creating a IntervalIndex
+interval_idx = pd.interval_range(start=0, end=4)
+# Creating a DataFrame with IntervalIndex
+df = pd.DataFrame({'Col1': [1, 2, 3, 4], 'Col2':[1, 3, 5, 8]}, index=interval_idx)
+print("Input DataFrame:\n",df)
+print("\nDataFrame Index Object Type:",df.index.dtype)
+print()
 # DatetimeIndex
 # TimedeltaIndex
 # PeriodIndex
