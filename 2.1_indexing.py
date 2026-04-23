11f0ce72e942b084960ac3a7f6d6393fe29c39a2
sreeram780 <sreenu.ram2@gmail.com>
2026-04-23 19:08:01 +0530

    numerical indexing

---
+++ 2.1_indexing.py
@@ -0,0 +1,28 @@
+# class pandas.Index(data=None, dtype=None, copy=False, name=None, tupleize_cols=True)
+# data: The data for the index, which can be an array-like structure (like a list or numpy array) or another index object.
+# dtype: It specifies the data type for the index values, If not provided, Pandas will decide the data type based on the index values.
+# copy: It is a boolean parameter (True or False), which, specifies to create a copy of the input data.
+# name: This parameter gives a label to the index.
+# data: It is also a boolean parameter (True or False), When True, it tries to create MultiIndex if possible.
+
+# NumericIndex
+import pandas as pd
+# Generate some data for DataFrame
+data = {
+   'Name': ['Steve', 'Lia', 'Vin', 'Katie'],
+   'Age': [32, 28, 45, 38],
+   'Gender': ['Male', 'Female', 'Male', 'Female'],
+   'Rating': [3.45, 4.6, 3.9, 2.78]
+}
+# Creating the DataFrame
+df = pd.DataFrame(data)
+# Display the DataFrame
+print(df)
+print("\nDataFrame Index Object Type:",df.index.dtype)
+print()
+# CategoricalIndex
+# MultiIndex
+# IntervalIndex
+# DatetimeIndex
+# TimedeltaIndex
+# PeriodIndex
