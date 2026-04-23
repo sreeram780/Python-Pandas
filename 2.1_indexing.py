668720cd032557441b317ca94b518a0678d978e1
sreeram780 <sreenu.ram2@gmail.com>
2026-04-23 19:11:23 +0530

    MultiIndex

--- 2.1_indexing.py
+++ 2.1_indexing.py
@@ -29,6 +29,15 @@ print("\nDataFrame Index Object Type:",df.index.dtype)
 print()
 
 # MultiIndex
+import pandas as pd
+# Create MultiIndex
+arrays = [[1, 1, 2, 2], ['red', 'blue', 'red', 'blue']]
+multi_idx = pd.MultiIndex.from_arrays(arrays, names=('number', 'color'))
+# Create a DataFrame with MultiIndex
+df = pd.DataFrame({'Col1': [1, 2, 3, 4], 'Col2':[1, 3, 5, 8]}, index=multi_idx)
+print("MultiIndexed DataFrame:\n",df)
+print()
+
 # IntervalIndex
 import pandas as pd
 # Creating a IntervalIndex
