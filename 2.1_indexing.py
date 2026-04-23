c2c7685cf9241d6b17c9fb2a60c9b44b56dac81b
sreeram780 <sreenu.ram2@gmail.com>
2026-04-23 19:14:09 +0530

    PeriodIndex

--- 2.1_indexing.py
+++ 2.1_indexing.py
@@ -67,3 +67,9 @@ print("TimedeltaIndexed DataFrame:\n",df)
 print()
 
 # PeriodIndex
+import pandas as pd
+# Create PeriodIndex
+period_idx = pd.PeriodIndex(year=[2020, 2024], quarter=[1, 3])
+# Create a DataFrame with PeriodIndex
+df = pd.DataFrame({'Col1': [1, 2], 'Col2':[1, 3]}, index=period_idx )
+print("PeriodIndexed DataFrame:\n",df)
