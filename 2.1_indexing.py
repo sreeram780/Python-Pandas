7f4ec85306e46d04654f6e5ea0bb5c508710963f
sreeram780 <sreenu.ram2@gmail.com>
2026-04-23 19:13:21 +0530

    TimedeltaIndex

--- 2.1_indexing.py
+++ 2.1_indexing.py
@@ -58,4 +58,12 @@ print("DatetimeIndexed DataFrame:\n",df)
 print()
 
 # TimedeltaIndex
+import pandas as pd
+# Create TimedeltaIndex
+timedelta_idx = pd.TimedeltaIndex(['0 days', '1 days', '2 days'])
+# Create a DataFrame with TimedeltaIndex
+df = pd.DataFrame({'Col1': [1, 2, 3], 'Col2':[1, 3, 3]}, index=timedelta_idx )
+print("TimedeltaIndexed DataFrame:\n",df)
+print()
+
 # PeriodIndex
