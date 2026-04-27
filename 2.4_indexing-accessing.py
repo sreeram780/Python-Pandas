7fe63c4e68d296396e82890cca15ac2e9cd97839
sreeram780 <sreenu.ram2@gmail.com>
2026-04-27 13:36:19 +0530

    Direct Indexing with Brackets []

--- 2.4_indexing-accessing.py
+++ 2.4_indexing-accessing.py
@@ -45,3 +45,13 @@ df = pd.DataFrame(np.random.randn(8, 4), columns = ['A', 'B', 'C', 'D'])
 print(df.iloc[[1, 3, 5], [1, 3]])
 
 # Indexing with Brackets []
+import pandas as pd
+import numpy as np
+
+df = pd.DataFrame(np.random.randn(8, 4), columns = ['A', 'B', 'C', 'D'])
+# Accessing a Single Column
+print(df['A'])
+
+df = pd.DataFrame(np.random.randn(8, 4), columns = ['A', 'B', 'C', 'D'])
+# Accessing Multiple Columns
+print(df[['A', 'B']])
