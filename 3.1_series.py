bc7c2664fe60aa4a0d2f3b2f956a7efc22b3a284
sreeram780 <sreenu.ram2@gmail.com>
2026-04-28 11:53:00 +0530

    Create an Empty Series

---
+++ 3.1_series.py
@@ -0,0 +1,8 @@
+# class pandas.Series(data, index, dtype, name, copy)
+# Create an Empty Series
+#import the pandas library and aliasing as pd
+import pandas as pd
+s = pd.Series()
+
+# Display the result
+print('Resultant Empty Series:\n',s)
