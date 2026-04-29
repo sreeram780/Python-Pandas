fe9746bcde71db31d90956d5ba8acc097270f305
sreeram780 <sreenu.ram2@gmail.com>
2026-04-29 09:47:45 +0530

    Modifying Values after Slicing

--- 3.2_series_slicing.py
+++ 3.2_series_slicing.py
@@ -38,3 +38,11 @@ print(s['a':'d'])
 s = pd.Series([1,2,3,4,5],index = ['a','b','c','d','e'])
 # Slice multiple elements
 print(s[:'c'])
+
+# Modifying Values after Slicing
+s = pd.Series([1,2,3,4,5])
+# Display the original series
+print("Original Series:\n",s)
+# Modify the values of first two elements
+s[:2] = [100, 200]
+print("Series after modifying the first two elements:",s)
