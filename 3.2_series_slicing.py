6b4ec1ec173c29ba33e1942811868dc490e1f293
sreeram780 <sreenu.ram2@gmail.com>
2026-04-29 09:46:03 +0530

    Slicing Group of elements from a Series using the Labels

--- 3.2_series_slicing.py
+++ 3.2_series_slicing.py
@@ -27,3 +27,9 @@ print(s[:3])
 s = pd.Series([1,2,3,4,5],index = ['a','b','c','d','e'])
 #retrieve the last three element
 print(s[-3:])
+
+# # Slicing a Series by Label
+# Example: Slicing Group of elements from a Series using the Labels
+s = pd.Series([1,2,3,4,5],index = ['a','b','c','d','e'])
+# Slice multiple elements
+print(s['a':'d'])
