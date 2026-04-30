1aaa61c0ee76507efa10a3239d6de044050c85e6
sreeram780 <sreenu.ram2@gmail.com>
2026-04-30 12:31:16 +0530

    Arithmetic Operations Between Two Series

--- 3.4_series_operation.py
+++ 3.4_series_operation.py
@@ -14,3 +14,14 @@ print('\nDivision:\n', s/2)
 print('\nExponentiation:\n', s**2)
 print('\nModulus:\n', s%2)
 print('\nFloor Division:\n', s//2)
+
+# Arithmetic Operations Between Two Series
+
+import pandas as pd
+s1 = pd.Series([1,2,3,4,5],index = ['a','b','c','d','e'])
+s2 = pd.Series([9, 8, 6, 5], index=['x','a','b','c'])
+# Apply all Arithmetic Operations and Display the Results
+print('\nAddition:\n',s1+s2)
+print('\nSubtraction:\n', s1-s2)
+print('\nMultiplication:\n', s1 * s2)
+print('\nDivision:\n', s1/s2)
