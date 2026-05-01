a71aeb72dc59649b47902ee910890effbfa77464
sreeram780 <sreenu.ram2@gmail.com>
2026-05-01 21:14:42 +0530

    series to numpy array

--- 3.5_series2objects.py
+++ 3.5_series2objects.py
@@ -13,3 +13,10 @@ s = pd.Series([1, 2, 3])
 result = s.to_list()
 print("Output:",result)
 print("Output Type:", type(result))
+
+# Create a Pandas Series
+s = pd.Series([1, 2, 3])
+# Convert Series to a NumPy Array
+result = s.to_numpy()
+print("Output:",result)
+print("Output Type:", type(result))
