2975d61dc60e440562be1b81beb5ce0c928655db
sreeram780 <sreenu.ram2@gmail.com>
2026-05-01 21:16:44 +0530

    Series to string representation

--- 3.5_series2objects.py
+++ 3.5_series2objects.py
@@ -34,3 +34,10 @@ s = pd.Series([1, 2, 3], index=['a', 'b', 'c'])
 result = s.to_frame(name='Numbers')
 print("Output:\n",result)
 print("Output Type:", type(result))
+
+# Create a Pandas Series
+s = pd.Series([1, 2, 3], index=['r1', 'r2', 'r3'])
+# Convert Series to string representation
+result = s.to_string()
+print("Output:",repr(result))
+print("Output Type:", type(result))
