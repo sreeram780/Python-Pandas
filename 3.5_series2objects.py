8bb231077f3c19933d2ea669561888c88f178b8d
sreeram780 <sreenu.ram2@gmail.com>
2026-05-01 21:15:27 +0530

    series to dictionary

--- 3.5_series2objects.py
+++ 3.5_series2objects.py
@@ -20,3 +20,10 @@ s = pd.Series([1, 2, 3])
 result = s.to_numpy()
 print("Output:",result)
 print("Output Type:", type(result))
+
+# Create a Pandas Series
+s = pd.Series([1, 2, 3], index=['a', 'b', 'c'])
+# Convert Series to a Python dictionary
+result = s.to_dict()
+print("Output:",result)
+print("Output Type:", type(result))
