6ca61188f641d6eb99006cbbac38d56d594f3315
sreeram780 <sreenu.ram2@gmail.com>
2026-05-02 08:26:44 +0530

    indexed DataFrame using arrays

--- 4.1_pandas.py
+++ 4.1_pandas.py
@@ -16,3 +16,8 @@ print(df)
 data = {'Name':['Tom', 'Jack', 'Steve', 'Ricky'],'Age':[28,34,29,42]}
 df = pd.DataFrame(data)
 print(df)
+
+# indexed DataFrame using arrays
+data = {'Name':['Tom', 'Jack', 'Steve', 'Ricky'],'Age':[28,34,29,42]}
+df = pd.DataFrame(data, index=['rank1','rank2','rank3','rank4'])
+print(df)
