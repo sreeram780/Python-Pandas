c6cc78bbced01673e1158b4ffa5bda86ad8466af
sreeram780 <sreenu.ram2@gmail.com>
2026-05-02 08:24:31 +0530

    Create a DataFrame from Lists

---
+++ 4.1_pandas.py
@@ -0,0 +1,9 @@
+#import the pandas library and aliasing as pd
+import pandas as pd
+df = pd.DataFrame()
+print(df)
+
+# Create a DataFrame from Lists
+data = [1,2,3,4,5]
+df = pd.DataFrame(data)
+print(df)
