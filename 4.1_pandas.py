59e4f4a0ab3ae3db12d71ecc5f6c6d3830e32d41
sreeram780 <sreenu.ram2@gmail.com>
2026-05-02 08:30:02 +0530

    DataFrame from Dict of Series

--- 4.1_pandas.py
+++ 4.1_pandas.py
@@ -42,3 +42,7 @@ d = {'one' : pd.Series([1, 2, 3], index=['a', 'b', 'c']),
    'two' : pd.Series([1, 2, 3, 4], index=['a', 'b', 'c', 'd'])}
 df = pd.DataFrame(d)
 print(df)
+
+data = pd.Series([1, 2, 3, 4], index=['a', 'b', 'c', 'd'])
+df = pd.DataFrame(data)
+print(df)
