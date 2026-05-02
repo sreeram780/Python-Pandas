576f56ff66c28c88d10c9df3d0866c756a997ac6
sreeram780 <sreenu.ram2@gmail.com>
2026-05-02 08:28:27 +0530

    create a DataFrame with a list of dictionaries, row indices, and column indices

--- 4.1_pandas.py
+++ 4.1_pandas.py
@@ -26,3 +26,12 @@ print(df)
 data = [{'a': 1, 'b': 2},{'a': 5, 'b': 10, 'c': 20}]
 df = pd.DataFrame(data)
 print(df)
+
+# create a DataFrame with a list of dictionaries, row indices, and column indices
+data = [{'a': 1, 'b': 2},{'a': 5, 'b': 10, 'c': 20}]
+#With two column indices, values same as dictionary keys
+df1 = pd.DataFrame(data, index=['first', 'second'], columns=['a', 'b'])
+#With two column indices with one index with other name
+df2 = pd.DataFrame(data, index=['first', 'second'], columns=['a', 'b1'])
+print(df1)
+print(df2)
