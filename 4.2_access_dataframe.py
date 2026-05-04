9085f1579eb90c917b956c0ea0814f869a3bfde0
sreeram780 <sreenu.ram2@gmail.com>
2026-05-04 09:50:16 +0530

    Modify the Column labels of the DataFrame

--- 4.2_access_dataframe.py
+++ 4.2_access_dataframe.py
@@ -34,3 +34,16 @@ df = pd.DataFrame({
 # Access the column labels of the DataFrame
 result = df.columns
 print('Output Accessed column Labels:', result)
+
+# Create a DataFrame
+df = pd.DataFrame({
+    'Name': ['Steve', 'Lia', 'Vin', 'Katie'],
+    'Age': [32, 28, 45, 38],
+    'Gender': ['Male', 'Female', 'Male', 'Female'],
+    'Rating': [3.45, 4.6, 3.9, 2.78]},
+    index=['r1', 'r2', 'r3', 'r4'])
+# Display the Input DataFrame
+print('Input DataFrame:\n', df)
+# Modify the Column labels of the DataFrame
+df.columns = ['Col1', 'Col2', 'Col3', 'Col4']
+print('Output Modified DataFrame with the updated Column Labels\n:', df)
