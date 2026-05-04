eb4cb8609478b2ff739d581b8d1526f86d24c3af
sreeram780 <sreenu.ram2@gmail.com>
2026-05-04 09:49:09 +0530

    Modify the Row labels of the DataFrame

--- 4.2_access_dataframe.py
+++ 4.2_access_dataframe.py
@@ -9,3 +9,16 @@ df = pd.DataFrame({
 # Access the rows of the DataFrame
 result = df.index
 print('Output Accessed Row Labels:', result)
+
+# Create a DataFrame
+df = pd.DataFrame({
+    'Name': ['Steve', 'Lia', 'Vin', 'Katie'],
+    'Age': [32, 28, 45, 38],
+    'Gender': ['Male', 'Female', 'Male', 'Female'],
+    'Rating': [3.45, 4.6, 3.9, 2.78]},
+    index=['r1', 'r2', 'r3', 'r4'])
+# Display the Input DataFrame
+print('Input DataFrame:\n', df)
+# Modify the Row labels of the DataFrame
+df.index = [100, 200, 300, 400]
+print('Output Modified DataFrame with the updated index labels:\n', df)
