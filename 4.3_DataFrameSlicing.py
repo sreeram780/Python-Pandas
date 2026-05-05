8671528a318676dfe3743106d83871e55ec22068
sreeram780 <sreenu.ram2@gmail.com>
2026-05-05 09:41:53 +0530

    Column Slicing using iloc[]

--- 4.3_DataFrameSlicing.py
+++ 4.3_DataFrameSlicing.py
@@ -19,3 +19,15 @@ print(df)
 result = df.loc['r1':'r3', 'col1']
 print("Output:")
 print(result)
+
+# Column Slicing using iloc[]
+data = {'A': [1, 2, 3], 'B': [4, 5, 6], 'C': [7, 8, 9]}
+df = pd.DataFrame(data)
+# Slice a single column
+col_A = df.iloc[:, 0]
+print("Slicing a single column A using iloc[]:")
+print(col_A)
+# Slice multiple columns
+cols_AB = df.iloc[:, 0:2]
+print("Slicing multiple columns A and B using iloc[]:")
+print(cols_AB)
