21451f52370f8e46df80799ca36d4e6b32f4d687
sreeram780 <sreenu.ram2@gmail.com>
2026-05-05 09:42:50 +0530

    Column Slicing Using loc[]

--- 4.3_DataFrameSlicing.py
+++ 4.3_DataFrameSlicing.py
@@ -31,3 +31,15 @@ print(col_A)
 cols_AB = df.iloc[:, 0:2]
 print("Slicing multiple columns A and B using iloc[]:")
 print(cols_AB)
+
+# Column Slicing Using loc[]
+data = {'A': [1, 2, 3], 'B': [4, 5, 6], 'C': [7, 8, 9]}
+df = pd.DataFrame(data)
+# Slice a single column by label
+col_A = df.loc[:, 'A']
+print("Slicing a single column A using loc[]:")
+print(col_A)
+# Slice multiple columns by label
+cols_AB = df.loc[:, 'A':'B']
+print("Slicing Multiple columns A and B using loc[]:")
+print(cols_AB)
