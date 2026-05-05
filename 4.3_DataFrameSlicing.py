264c03744b405415a44a94b723e275b9e31d0083
sreeram780 <sreenu.ram2@gmail.com>
2026-05-05 09:40:40 +0530

    Slice rows and columns by label

--- 4.3_DataFrameSlicing.py
+++ 4.3_DataFrameSlicing.py
@@ -9,3 +9,13 @@ print(df)
 result = df.iloc[1:3, :]
 print("Output:")
 print(result)
+
+# Create a DataFrame with labeled indices
+df = pd.DataFrame([['a','b'], ['c','d'], ['e','f'], ['g','h']], columns=['col1', 'col2'], index=['r1', 'r2', 'r3', 'r4'])
+# Display the DataFrame
+print("Original DataFrame:")
+print(df)
+# Slice rows and columns by label
+result = df.loc['r1':'r3', 'col1']
+print("Output:")
+print(result)
