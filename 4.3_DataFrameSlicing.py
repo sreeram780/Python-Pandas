c5ad9c40a251e5733f28601534811aaeffb9ceba
sreeram780 <sreenu.ram2@gmail.com>
2026-05-06 17:54:40 +0530

    Replacing a Columns Values

--- 4.3_DataFrameSlicing.py
+++ 4.3_DataFrameSlicing.py
@@ -97,3 +97,12 @@ df.insert(1, 'D', [10, 11, 12])
 # Display updated DataFrame
 print("DataFrame after inserting column 'D' at position 1:")
 print(df)
+
+# Replacing a Columns Values
+# Create a DataFrame
+df = pd.DataFrame({'A': [1, 2, 3],'B': [4, 5, 6]})
+# Replace the contents of column 'A' with new values
+df['A'] = [10, 20, 30]
+# Display updated DataFrame
+print("DataFrame after replacing column 'A':")
+print(df)
