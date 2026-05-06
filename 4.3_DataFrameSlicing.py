b5bdf734f49aa47f353e05558e731277f6d9bc77
sreeram780 <sreenu.ram2@gmail.com>
2026-05-06 17:55:20 +0530

    Replacing Specific Values using the replace() method

--- 4.3_DataFrameSlicing.py
+++ 4.3_DataFrameSlicing.py
@@ -106,3 +106,14 @@ df['A'] = [10, 20, 30]
 # Display updated DataFrame
 print("DataFrame after replacing column 'A':")
 print(df)
+
+# Replacing Specific Values using the replace() method
+# Create a DataFrame
+df = pd.DataFrame({'A': [1, 2, 3],'B': [4, 5, 6]})
+# Display the Input DataFrame
+print("Original DataFrame:", df, sep='\n')
+# Replace the contents
+df.replace({'A': 1, 'B': 6}, 100, inplace=True)
+# Display updated DataFrame
+print("DataFrame after replacing column 'A':")
+print(df)
