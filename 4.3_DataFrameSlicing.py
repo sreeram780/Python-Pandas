37b4134daaf98b9b4b2ff25f0b454cc8a711308c
sreeram780 <sreenu.ram2@gmail.com>
2026-05-06 17:55:58 +0530

    Deleting Columns

--- 4.3_DataFrameSlicing.py
+++ 4.3_DataFrameSlicing.py
@@ -117,3 +117,14 @@ df.replace({'A': 1, 'B': 6}, 100, inplace=True)
 # Display updated DataFrame
 print("DataFrame after replacing column 'A':")
 print(df)
+
+# Deleting Columns
+# Create a DataFrame
+df = pd.DataFrame({'A': [1, 2, 3],'B': [4, 5, 6],'C': [7, 8, 9]})
+# Display the original DataFrame
+print("Original DataFrame:", df, sep='\n')
+# Delete columns 'A' and 'B'
+df = df.drop(columns=['A', 'B'])
+# Display updated DataFrame
+print("DataFrame after deleting columns 'A' and 'B':")
+print(df)
