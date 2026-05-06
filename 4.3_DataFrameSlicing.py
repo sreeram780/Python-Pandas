b6a15b0b2a27335be47d24a3e3975758e5623f50
sreeram780 <sreenu.ram2@gmail.com>
2026-05-06 17:52:54 +0530

    Renaming Row Labels in a DataFrame

--- 4.3_DataFrameSlicing.py
+++ 4.3_DataFrameSlicing.py
@@ -67,3 +67,15 @@ df = df.rename(columns={'A': 'aa'})
 # Display modified DataFrame
 print("Modified DataFrame:")
 print(df)
+
+# Renaming Row Labels in a DataFrame
+# Create a DataFrame
+df = pd.DataFrame({'A': [1, 2, 3],'B': [4, 5, 6]}, index=['x', 'y', 'z'])
+# Display original DataFrame
+print("Original DataFrame:")
+print(df)
+# Rename the multiple row labels
+df = df.rename(index={'x': 'r1', 'y':'r2', 'z':'r3'})
+# Display modified DataFrame
+print("Modified DataFrame:")
+print(df)
