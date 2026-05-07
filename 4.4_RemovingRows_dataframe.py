0538ab7ef8d039c8618a3a976e95b8d843e49ed6
sreeram780 <sreenu.ram2@gmail.com>
2026-05-07 16:18:40 +0530

    Removing Rows Based on a Condition

--- 4.4_RemovingRows_dataframe.py
+++ 4.4_RemovingRows_dataframe.py
@@ -25,3 +25,16 @@ result = df.drop(['r1', 'r3'])
 # Display the result
 print("\nAfter dropping the rows:")
 print(result)
+
+# Removing Rows Based on a Condition
+# Create a DataFrame
+df = pd.DataFrame({'A': [1, 2, 3, 4, 5],'B': [4, 5, 6, 7, 8],
+'C': [90, 0, 11, 12, 13]}, index=['r1', 'r2', 'r3', 'r4', 'r5'])
+# Display original DataFrame
+print("Original DataFrame:")
+print(df)
+# Dropping rows where column 'C' contains 0
+result = df[df["C"] != 0]
+# Display the result
+print("\nAfter dropping the row where 'C' has 0:")
+print(result)
