f1f0ab8bf53685cb06f6161275b514e1f323c389
sreeram780 <sreenu.ram2@gmail.com>
2026-05-07 16:17:17 +0530

    Dropping Multiple Rows by Labels

--- 4.4_RemovingRows_dataframe.py
+++ 4.4_RemovingRows_dataframe.py
@@ -12,3 +12,16 @@ result = df.drop(3)
 # Display the result
 print("\nAfter dropping the row at index 3:")
 print(result)
+
+# Dropping Multiple Rows by Labels
+# Create a DataFrame
+df = pd.DataFrame({'A': [1, 2, 3, 4, 5],'B': [4, 5, 6, 7, 8],
+'C': [9, 10, 11, 12, 13]}, index=['r1', 'r2', 'r3', 'r4', 'r5'])
+# Display original DataFrame
+print("Original DataFrame:")
+print(df)
+# Drop the rows by row-labels
+result = df.drop(['r1', 'r3'])
+# Display the result
+print("\nAfter dropping the rows:")
+print(result)
