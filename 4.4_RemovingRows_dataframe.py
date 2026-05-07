6866fdfd7419b70d0d577b9ccdadcc9027104742
sreeram780 <sreenu.ram2@gmail.com>
2026-05-07 16:19:52 +0530

    Removing Rows using Index Slicing

--- 4.4_RemovingRows_dataframe.py
+++ 4.4_RemovingRows_dataframe.py
@@ -38,3 +38,16 @@ result = df[df["C"] != 0]
 # Display the result
 print("\nAfter dropping the row where 'C' has 0:")
 print(result)
+
+# Removing Rows using Index Slicing
+
+# Create a DataFrame
+df = pd.DataFrame({'A': [1, 2, 3, 4, 5],'B': [4, 5, 6, 7, 8]})
+# Display original DataFrame
+print("Original DataFrame:")
+print(df)
+# Drop the row using the index slicing
+result = df.drop(df.index[2:4])
+# Display the result
+print("\nAfter dropping the row at 2 and 3:")
+print(result)
