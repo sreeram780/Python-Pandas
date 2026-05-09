54a475b429a97e4afd4169a3b6c4970b821eca94
sreeram780 <sreenu.ram2@gmail.com>
2026-05-09 21:41:41 +0530

    Arithmetic Operations Between Two DataFrames

--- 4.5_arithmatic_operations_dataframe.py
+++ 4.5_arithmatic_operations_dataframe.py
@@ -15,3 +15,16 @@ print("\nDivision:\n", df / 2)
 print("\nExponentiation:\n", df ** 2)
 print("\nModulus:\n", df % 2)
 print("\nFloor Division:\n", df // 2)
+
+# Arithmetic Operations Between Two DataFrames
+# Create two DataFrames
+df1 = pd.DataFrame({'A': [1, 2, 3, 4], 'B': [5, 6, 7, 8]})
+df2 = pd.DataFrame({'A': [10, 20, 30], 'B': [50, 60, 70]}, index=[1, 2, 3])
+# Display the input DataFrames
+print("DataFrame 1:\n", df1)
+print("\nDataFrame 2:\n", df2)
+# Perform arithmetic operations
+print("\nAddition of Two DataFrames:\n", df1 + df2)
+print("\nSubtraction of Two DataFrames:\n", df1 - df2)
+print("\nMultiplication of Two DataFrames:\n", df1 * df2)
+print("\nDivision of Two DataFrames:\n", df1 / df2)
